a8e07d42192e5ce4346691150b330d4c89d25880
jhon <pioneer3692@gmail.com>
2021-11-16 20:17:10 +0900

    add profile session

--- hackershack_website/views.py
+++ hackershack_website/views.py
@@ -1,17 +1,24 @@
 # from django.http import HttpResponse
 # from django.template import loader
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 def index(request):
     # print(f"request is {request}")
-    return render(request, 'index.html')
+    return render(request, "index.html")
     # template = loader.get_template('index.html')
     # return HttpResponse(template.render({}, request))
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, "contact.html")
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "accounts/profile.html"
